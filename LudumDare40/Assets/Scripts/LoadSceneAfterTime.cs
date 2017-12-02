19a6f3677ad937eca6d9019a4330781ed9a27430
Antoine Berthiaume Dutil <antoineberthiaumedutil@gmail.com>
2017-12-01 19:48:57 -0500

    First Push!!!

---
+++ LudumDare40/Assets/Scripts/LoadSceneAfterTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAfterTime : MonoBehaviour {
+
+    public float timeBeforeLoading = 5f;
+    public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("loadScene", timeBeforeLoading);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void loadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
